43b92fe4618152dd6c896ac2b583b6d0c56d5273
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-02-19 12:53:32 +0200

    portfolio update

--- Portfolio 2.0/main.js
+++ Portfolio 2.0/main.js
@@ -1,13 +1,39 @@
-let header = document.getElementById("main_nav");
+let header = document.querySelector("header");
+let buttonBackToTop = document.getElementById("backToTop");
 
-const scrollHeader = () => {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    header.style.background = "#F4F2FD";
-    header.style.padding = "10px";
+window.onscroll = function () {
+  headerFunction();
+};
+
+const headerFunction = () => {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    header.classList.add("bg");
   } else {
-    header.style.background = "transparent";
-    header.style.padding = "42px";
+    header.classList.remove("bg");
   }
 };
 
-window.addEventListener("scroll", scrollHeader);
+window.onscroll = function () {
+  scrollFunction();
+};
+
+const scrollFunction = () => {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    buttonBackToTop.style.display = "block";
+  } else {
+    buttonBackToTop.style.display = "none";
+  }
+};
+
+const getToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+buttonBackToTop.addEventListener("click", getToTop);
